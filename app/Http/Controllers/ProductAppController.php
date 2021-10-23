2efdda37a5a90b793b8b6916353b14ed5303e24b
Proshtugh <supratimghosh2772@gmail.com>
2021-10-23 15:24:03 +0530

    Update ProductAppController.php

--- app/Http/Controllers/ProductAppController.php
+++ app/Http/Controllers/ProductAppController.php
@@ -26,4 +26,20 @@ class ProductAppController extends BaseController
         */
         return $products;
     }
+	
+	public function productlist_4($id)
+    {
+		/*
+		$products = Product::where('category',$id)->get();
+		*/
+		
+		$products = DB::table('products')->select('id','name','size','price','discount','brand','stock',DB::raw('0 as qty'))
+		->where('category',$id)->take(4)->get();
+		
+		/*
+		$products = DB::table('products')->join('file_uploads','products.id','=','file_uploads.productId')
+		->select('products.id','products.name','file_uploads.filename')->where('category',$id)->get();		
+        */
+        return $products;
+    }
 }
